b3ac61044de8f040a58e133504a9cd63a6461842
VatsalPatel18 <vatsal1804@gmail.com>
2024-03-27 12:32:35 +0530

    Initial commit

---
+++ .ipynb_checkpoints/PlipDataProcess-checkpoint.py
@@ -0,0 +1,48 @@
+import os
+import random
+import torch
+from PIL import Image
+from concurrent.futures import ThreadPoolExecutor
+
+class PlipDataProcess(torch.utils.data.Dataset):
+    def __init__(self, root_dir, files, df, img_processor=None, num_tiles_per_patient=128, max_workers=64, save_dir='processed_tile_data'):
+        self.root_dir = root_dir
+        self.files = files
+        self.df = df
+        self.img_processor = img_processor
+        self.num_tiles_per_patient = num_tiles_per_patient
+        self.max_workers = max_workers
+        self.save_dir = save_dir
+        if not os.path.exists(self.save_dir):
+            os.makedirs(self.save_dir)
+
+    def __len__(self):
+        return len(self.files)
+
+    def load_and_process_image(self, tile_path):
+        image = Image.open(tile_path)
+        return self.img_processor.preprocess(image)['pixel_values']
+
+    def save_individual_tile_data(self, tile_data, file_data, file_name, tile_name):
+        save_path = os.path.join(self.save_dir, file_name, f"{tile_name}.pt")
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        torch.save({'tile_data': tile_data, 'file_data': file_data}, save_path)
+
+    def __getitem__(self, idx):
+        file = self.files[idx]
+        tiles_path = os.path.join(self.root_dir, file, file)
+        tiles = [tile for tile in os.listdir(tiles_path) if tile != '.ipynb_checkpoints']
+        selected_tiles = random.sample(tiles, min(self.num_tiles_per_patient, len(tiles)))
+
+        file_data = torch.tensor(self.df.loc[f'{file}-01'].values, dtype=torch.float32)
+
+        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+            for tile_name in selected_tiles:
+                tile_path = os.path.join(tiles_path, tile_name)
+                executor.submit(self.process_and_save_tile, tile_path, file_data, file, tile_name)
+
+        return idx
+
+    def process_and_save_tile(self, tile_path, file_data, file_name, tile_name):
+        tile_data = self.load_and_process_image(tile_path)
+        self.save_individual_tile_data(tile_data, file_data, file_name, tile_name)
---
+++ .ipynb_checkpoints/process_tiles-checkpoint.py
@@ -0,0 +1,43 @@
+import os
+import pandas as pd
+from PlipDataProcess import PlipDataProcess
+from transformers import CLIPImageProcessor
+import argparse
+
+def main(csv_file, root_dir, save_dir):
+    df4 = pd.read_csv(csv_file).set_index('PatientID')
+    there = set(list(x[:-3] for x in df4.index))
+
+    wsi_data = {}
+    for file in os.listdir(root_dir):
+        fol_p = os.path.join(root_dir, file, file)
+        f2lp_t = len(os.listdir(fol_p))
+        wsi_data[file] = f2lp_t
+
+    wsi_there = set(wsi_data.keys())
+    use = list(there.intersection(wsi_there))
+
+    img_processor = CLIPImageProcessor.from_pretrained("./plip/")
+
+    dataset = PlipDataProcess(
+        root_dir=root_dir,
+        files=use,
+        df=df4,
+        img_processor=img_processor,
+        num_tiles_per_patient=2000,
+        max_workers=64,
+        save_dir=save_dir
+    )
+
+    for i in range(len(dataset)):
+        _ = dataset[i] 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Process WSI images and generate tiles")
+    parser.add_argument('--csv_file', type=str, required=True, help='Path to the CSV file with patient scores')
+    parser.add_argument('--root_dir', type=str, required=True, help='Root directory for WSI tiles')
+    parser.add_argument('--save_dir', type=str, required=True, help='Directory to save the processed tile data')
+
+    args = parser.parse_args()
+
+    main(csv_file=args.csv_file, root_dir=args.root_dir, save_dir=args.save_dir)
